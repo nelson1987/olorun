7affd2fdee06e47bb92f3bd1c3ad968474fb7a9d
Nelson Neto <nelson.ash@outlook.com>
2024-01-15 12:09:49 -0300

    Update docker-compose files, Dockerfile, Program.cs, appsettings.json, and add new files. (generated by blackbox.ai )

--- src/Pagamento/Program.cs
+++ src/Pagamento/Program.cs
@@ -1,9 +1,17 @@
+using Pagamento.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Mongo
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+//Redis
+//Kafka
+builder.Services.AddSingleton<IRepository, Repository>();
 
 var app = builder.Build();
 
@@ -14,31 +22,25 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (IRepository repository) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
+    var clima = new WeatherForecast
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            DateOnly.FromDateTime(DateTime.Now.AddDays(1)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+        );
+    await repository.CreateAsync(clima);
+    return await repository.GetAsync();
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- src/SharedDomain/Class1.cs
+++ src/SharedDomain/Class1.cs
@@ -1,6 +1,40 @@
 ﻿namespace SharedDomain;
 
-public class Class1
+public record Leilao
 {
-
+    public Guid Id { get; init; }
+    public required Produto Objeto { get; init; }
+    public required List<Lance> Lances { get; init; }
+    public void DarLance(Lance lance)
+    {
+        Lances.Add(lance);
+    }
 }
+public record Produto
+{
+    public required string Descricao { get; init; }
+}
+public record Cliente
+{
+    public required string Email { get; init; }
+}
+public record Lance
+{
+    public required Cliente Cliente { get; init; }
+    public required decimal Valor { get; init; }
+}
+//Comando
+public record IncluirLanceCommand(Guid IdLeilao, string email, decimal valor);
+//Eventos
+public record LanceAceitoEvent();
+public record LanceRejeitadoEvent();
+
+public class LanceCommandHandler
+{
+    public Result Handle(IncluirLanceCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Result { }
