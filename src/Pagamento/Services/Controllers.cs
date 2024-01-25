ff7954db8cc80e82682f459ccc3e27db918ddca8
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-01-25 10:41:37 -0300

    controller

---
+++ src/Pagamento/Services/Controllers.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Pagamento.Features;
+using Pagamento.Features.Entities;
+
+namespace Pagamento.Services
+{
+    public static class Controllers
+    {
+        public static IEndpointRouteBuilder AddweatherforecastEndpoints(this IEndpointRouteBuilder app)
+        {
+            var todoItems = app.MapGroup("api/v1/weatherforecasts")
+                    //.RequireAuthorization()
+                    .WithTags("Weatherforecast");
+
+            todoItems.MapGet("/", GetWeatherForecast)
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+            todoItems.MapPut("/{id}", GetWeatherForecastById)
+            .WithName("PutWeatherForecast")
+            .WithOpenApi();
+
+            todoItems.MapPost("/", async ([FromServices] IWeatherForecastHandler handler, CancellationToken cancellationToken) =>
+            {
+                await handler.PostAsync(cancellationToken);
+            })
+            .WithName("PostWeatherForecast")
+            .WithOpenApi();
+
+            todoItems.MapDelete("/{id}", async ([FromServices] IWeatherForecastHandler handler, Guid id, CancellationToken cancellationToken) =>
+            {
+                await handler.DeleteAsync(id, cancellationToken);
+            })
+            .WithName("DeleteWeatherForecast")
+            .WithOpenApi();
+
+            return app;
+        }
+        static async Task<IResult> GetWeatherForecast(IWeatherForecastHandler handler, CancellationToken cancellationToken) =>
+            await handler.GetAsync(cancellationToken) is IList<WeatherForecast> todo
+            ? TypedResults.Ok(todo)
+            : TypedResults.NotFound();
+
+        static async Task<IResult> GetWeatherForecastById(Guid id, IWeatherForecastHandler handler, CancellationToken cancellationToken) =>
+            await handler.PutAsync(id, cancellationToken) is WeatherForecast todo
+            ? TypedResults.Ok(todo)
+            : TypedResults.NotFound();
+    }
+}
