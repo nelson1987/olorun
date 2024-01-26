5b3e798045dadb7c99888f952002aa1e84a179a7
Nelson Neto <nelson.ash@outlook.com>
2024-01-26 11:47:22 -0300

    add event

---
+++ src/SharedDomain/Features/Events.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace SharedDomain.Features;
+public interface IEvent
+{
+}
+public interface IEntity
+{
+}
+public interface IHandler { }
+public class KafkaProducer<T> where T : IEvent
+{
+}
+public class KafkaConsume<T> where T : IEvent
+{
+}
+public record CreateTransferenciaEvent : IEvent
+{
+}
+public class CreateTransferenciaEventValidator : AbstractValidator<CreateTransferenciaEvent>
+{
+}
+public class Transferencia : IEntity
+{
+}
+public record CreateTransferenciaCommand
+{
+}
+public class CreateTransferenciaCommandValidator : AbstractValidator<CreateTransferenciaCommand>
+{
+}
+public class CreateTransferenciaHandler : IHandler
+{
+
+}
